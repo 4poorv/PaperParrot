c6066e310faece954b165d9f70e36e948ca5a268
Apoorv Jain <4poorv@gmail.com>
2023-05-18 19:34:53 +0530

    Add progress bar

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ load_dotenv()
 # Create instance of OpenAI LLM
 llm = OpenAI(temperature=0.1, verbose=True, api_key=getenv('OPENAI_API_KEY'))
 
-st.title('Investment Advisor')
+st.title('PaperParrot')
 
 # Create a file uploader widget
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
@@ -25,8 +25,17 @@ if uploaded_file is not None:
     with open(file_path, 'wb') as f:
         f.write(uploaded_file.getvalue())
 
+    st.success('Congratulations! PDF successfully uploaded.')
+
+    st.info("Hold on tight! We're loading the PDF and training ChatGPT")
+    progress = st.progress(0)
+
     # Load documents into vector database aka ChromaDB
     pages = PyPDFLoader(file_path).load_and_split()
+
+    # Update progress bar
+    progress.progress(50)
+
     store = Chroma.from_documents(pages, collection_name='annualreport')
 
     # Convert the document store into a langchain toolkit
@@ -39,10 +48,17 @@ if uploaded_file is not None:
     # Add the toolkit to an end-to-end LC
     agent_executor = create_vectorstore_agent(llm=llm, toolkit=toolkit, verbose=True)
 
-    if prompt := st.text_input('Input your prompt here'):
+    # Update progress bar to 100%
+    progress.progress(100)
+    st.success('PDF is loaded and our model is trained. Get ready to ask your side-splitting questions.')
+
+    # Print a summary of the PDF (e.g. the first page)
+    # st.info(f"PDF Summary:\n\n{pages[0]}")
+
+    if prompt := st.text_input('Fire away with your follow-up questions!'):
         st.write(agent_executor.run(prompt))  # Then pass the prompt to the LLM and write it out to the screen
 
         # With a streamlit expander
-        with st.expander('Document Similarity Search'):
+        with st.expander('Related Sections for Above Information'):
             # Find the relevant pages and Write out the first
             st.write(store.similarity_search_with_score(prompt)[0][0].page_content)
